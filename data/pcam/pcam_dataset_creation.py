40cb139c30670b4849c2bd0d26f4b2566460c2b8
Marko Bauer <bauer.marko.98@gmail.com>
2020-02-14 23:59:55 +0100

    distribute images to appropriate directories

--- data/pcam/pcam_dataset_creation.py
+++ data/pcam/pcam_dataset_creation.py
@@ -4,7 +4,7 @@ import shutil
 import random
 import pandas as pd
 
-'''
+
 with zipfile.ZipFile('histopathologic-cancer-detection.zip', 'r') as zip_ref:
     zip_ref.extractall(os.getcwd())
 
@@ -21,16 +21,29 @@ tissue_types = ['non_tumor_tissue', 'tumor_tissue']
 for directory in ['pcam_train', 'pcam_validation', 'pcam_test']:
     for tissue_type in tissue_types:
             os.mkdir(directory + '/' + tissue_type)
-'''
 
-tissue_types = ['non_tumor_tissue', 'tumor_tissue']
-
-total = len([name for name in os.listdir(os.getcwd() + '/' + 'train')])
-number_to_move = int(total*0.15)
 train_labels = pd.read_csv('train_labels.csv')
 
-#label = int(train_labels.loc[train_labels['id'] == 'a81f84895ddcd522302ddf34be02eb1b3e5af1cb']['label'])
-#print(label)
+#move data to train directory
+for image in os.listdir(os.getcwd() + '/train'):
+    label = int(train_labels.loc[train_labels['id'] == image[0:-4]]['label'])
+    shutil.move(os.getcwd() + '/train/' + image, os.getcwd() + '/pcam_train/' + tissue_types[label])
 
-def move_to_directory(directory, number_to_move):
-    
+os.remove('train_labels.csv')
+shutil.rmtree('train')
+
+#move data to validation and test directory
+def move_to_directory(directory, tissue_type, total, number_to_move):
+    files = [ f.path for f in os.scandir(os.getcwd() + '/pcam_train/' + tissue_type)]
+    random_list = random.sample(range(0, total-1), number_to_move)
+    files_to_move = []
+    for random_number in random_list:
+        files_to_move.append(files[random_number])
+    for file_path in files_to_move:
+        shutil.move(file_path, os.getcwd() + directory + tissue_type)
+
+for tissue_type in tissue_types:
+    total = len([name for name in os.listdir(os.getcwd() + '/pcam_train/' + tissue_type)])
+    number_to_move = int(total*0.15)
+    move_to_directory('/pcam_validation/', tissue_type, total, number_to_move)
+    move_to_directory('/pcam_test/', tissue_type, total-number_to_move, number_to_move)
