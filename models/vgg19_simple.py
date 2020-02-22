dc0294ce1dc81dcd35c5fc2331b8ac5fa0931003
Marko Bauer <bauer.marko.98@gmail.com>
2020-02-22 20:33:22 +0100

    add build

--- models/vgg19_simple.py
+++ models/vgg19_simple.py
@@ -1,9 +1,11 @@
 import os
 from datetime import datetime
 
-from keras import models, layers
+from keras.layers import Flatten, Dropout, Dense
+from keras.models import Model
 from keras.applications import VGG19
 from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
 
 
 class VGG19Simple():
@@ -16,10 +18,9 @@ class VGG19Simple():
                  batch_size = 32,
                  classes = None,
                  weights = 'imagenet',
-                 incluce_top = False,
+                 include_top = False,
                  loss = 'categorical_crossentropy',
-                 learning_rate = 1e-4,
-                 optimizer = RMSprop(),
+                 optimizer = RMSprop(1e-4),
                  metrics = ['acc'],
                  epochs = 100):
 
@@ -28,9 +29,11 @@ class VGG19Simple():
             network_name: string, name of the network, default: 'VGG19Simple'
             dataset_name: string, name of the dataset, default: 'None'
             image_size: (int, int), size of the input images, default: None
-            data_augmentation: bool, use data augmentation or not, default: False
+            data_augmentation: bool, whether to use data augmentation, default: False
             batch_size: int, batch size during network training, default: 32
             classes: int, number of classes in dataset, default: None
+            weights: 'imagenet' (pre-training on ImageNet) or path to weights file to be loaded, defualt: 'imagenet'
+            include_top: bool, whether to include the 3 fully-connected layers at the top of the network, default: False
 
         '''
 
@@ -57,10 +60,15 @@ class VGG19Simple():
                 self.classes = len([ f.path for f in os.scandir(os.path.join(dataset_path, 'train')) if f.is_dir() ])
             except Exception as e:
                 print('Error caught in constructor: ', e)
-        self.create_data_generators()
+        self.data_generators()
+
+        self.weights = weights
+        self.include_top = include_top
+        self.build()
+        self.compile()
 
 
-    def create_data_generators():
+    def data_generators():
         if self.data_augmentation is True:
             train_datagen = ImageDataGenerator(rescale=1./255,
                                                rotation_range=45,
@@ -92,3 +100,24 @@ class VGG19Simple():
                 target_size = self.image_size,
                 batch_size = self.batch_size,
                 class_mode = 'categorical' if self.classes>1 else 'binary')
+
+
+        def build():
+            convolutional_base = VGG19(weights = self.weights,
+                                       include_top = self.include_top,
+                                       input_shape = (self.image_size, 3))
+
+            if self.include is False:
+                flatten = Flatten(name = 'flatten')(convolutional_base.outputs)
+                dropout_1 = Dropout(0.5, name = 'dropout_1')(flatten)
+                dense_1 = Dense(256, activation = 'relu', name = 'dense')(dropout_1)
+                dropout_2 = Dropout(0.5, name = 'dropout_2')(dense_1)
+                output = Dense(10, activation = 'softmax', name = 'predictions')(dropout_2)
+                model = Model(inputs = convolutional_base.inputs, outputs = output)
+                self.model = model
+            else:
+                self.model = convolutional_base
+
+
+        def compile():
+            convolutional_base = 5
